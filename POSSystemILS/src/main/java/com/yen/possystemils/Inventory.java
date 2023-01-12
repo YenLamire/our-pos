f6454b4f2f6b0ee928751bd26771191d0ca318ac
Yen Lamire <96825536+YenLamire@users.noreply.github.com>
2023-01-12 22:01:46 +0800

    Add files via upload

---
+++ POSSystemILS/src/main/java/com/yen/possystemils/Inventory.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.yen.possystemils;
+
+/**
+ *
+ * @author Me
+ */
+import java.util.*;
+public class Inventory {
+    public static Hashtable<String,Double> priceStorage = new Hashtable<String,Double>();
+    public static Hashtable<String,Integer> itemStocks = new Hashtable<String,Integer>();
+}
---
+++ POSSystemILS/src/main/java/com/yen/possystemils/MainInitializer.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.yen.possystemils;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author Me
+ */
+class POSSystemILS {
+
+    public static void main(String[] args) {
+        FormSelector x = new FormSelector();
+        x.setVisible(true);
+        x.setTitle("ILS ng Eh paano kung");
+    }
+}
